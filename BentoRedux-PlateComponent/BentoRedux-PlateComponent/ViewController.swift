5ea7a2622ec8eb00640b289c5862a55c300b2b4b
Romy Ilano <info@romyilano.com>
2017-01-29 13:10:40 -0800

    project added

---
+++ BentoRedux-PlateComponent/BentoRedux-PlateComponent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BentoRedux-PlateComponent
+//
+//  Created by Romy Ilano on 1/29/17.
+//  Copyright © 2017 Romy Ilano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
